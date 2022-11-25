5f90cbc6aded74a054b63164d996f48d33f8a013
Yasin Elbüz <yasinelbuz@gmail.com>
2022-11-25 17:23:57 +0300

    comment adding

--- main.js
+++ main.js
@@ -8,30 +8,43 @@ const knights = [
     { name: 'K6', heart: 100, alive: true },
 ];
 
+//başlangıç değişkenleri
 let damage = null;
 let count = 0;
 let win;
 
 while (true) {
 
-    //hasar number
+    //rastgele hasar
     damage = Math.floor(Math.random() * 7) + 1;
 
+    //saldıracak oyuncu yaşıyor mu?
     if (knights[count]?.alive) {
+
+        //gecici bir değişken
         let say = count;
+
         while (true) {
+
+            //hedef oyuncu yaşıyor mu?
             if (knights[say + 1]?.alive) {
                 knights[say + 1].heart -= damage;
                 console.log(`${knights[count].name} hits ${knights[say + 1].name} by ${damage} damage points`);
                 break;
             } else {
+
+                //eğer son oyuncuysa
                 if (knights.length == say) {
                     say = 0;
+
+                    //ilk baştaki oyuncu yaşıyor mu?
                     if (knights[say]?.alive) {
                         knights[say].heart -= damage;
                         console.log(`${knights[count].name} hits ${knights[say].name} by ${damage} damage points`);
                         break;
                     } else {
+
+                        //oyunu kontrol ediyoruz döngü bitti mi?
                         say += 1;
                         if (say === count) {
                             break;
@@ -45,19 +58,21 @@ while (true) {
     }
 
 
-
+    //canı bitenleri false yapıyoruz.
     if (knights[count]?.heart <= 0) {
         knights[count].alive = false;
     }
 
 
 
+    //kazananı bulmak için
     if (knights.filter(k => k.alive === true).length == 1) {
         win = knights.find(k => k.alive === true);
         console.log(`win ${knights[count].name}`);
         break;
     }
 
+    //son oyuncuya geldi mi?
     if (knights.length === count) {
         count = 0;
         console.log("--------------")
